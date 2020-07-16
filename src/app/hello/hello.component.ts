229778e4724be98c96a964c2852e8d59d78c7b72
oowada takuji <jiinyo829@gmail.com>
2020-07-16 19:57:26 +0900

    HelloCompnent修正

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -3,26 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hello',
   styleUrls: ['./hello.component.css'],
-  template: `
-    <div div id="body" >
-      <h1>
-          {{title}}
-      </h1>
-      <p>
-          {{ message }}
-      </p>
-    </div>
-  `
+  templateUrl: './hello.component.html'
 })
 export class HelloComponent implements OnInit {
   title: string;
   message: string;
+  switch: string;
 
   constructor() {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.title = 'Hello-app';
-    this.message = 'This is inline-template';
+    this.message = 'select menu:';
+    this.switch = "one";
+  }
+
+  doSelect(val) {
+    this.switch = val;
   }
 }
