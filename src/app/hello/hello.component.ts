c50ad538db01e52460a18fd79a791d229693d2c0
oowada takuji <jiinyo829@gmail.com>
2020-07-19 20:14:44 +0900

    Formのバリデーション設定

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hello',
@@ -12,20 +12,28 @@ export class HelloComponent implements OnInit {
   message: string;
   myControl: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor() { }
 
   ngOnInit() {
     this.title = 'Hello-app';
-    this.message = 'FormBuilderを使う';
-    this.myControl = this.fb.group({
-      name: [''],
-      mail: [''],
-      age: [0]
+    this.message = 'FormControlを使う';
+    this.myControl = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      mail: new FormControl('', [Validators.email]),
+      age: new FormControl(0, [Validators.min(1), Validators.max(150)])
     });
   }
 
+  get name() { return this.myControl.get('name'); }
+  get mail() { return this.myControl.get('mail'); }
+  get age() { return this.myControl.get('age'); }
+
   onSubmit() {
-    let result = this.myControl.value;
-    this.message = JSON.stringify(result);
+    if (this.myControl.invalid) {
+      this.message = "VALIDATION ERROR"
+    } else {
+      let result = this.myControl.value
+      this.message = JSON.stringify(result)
+    }
   }
 }
