a53cab4919c4bfea0838060c2c71699c4b7cb4b2
oowada takuji <jiinyo829@gmail.com>
2020-07-20 11:57:48 +0900

    バリデーション定義

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,6 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+//半角アルファベットのみ入力を許可
+function alpha(c: FormControl) {
+  let REGPATTERN = /^[a-zA-Z]+$/;
+  if (REGPATTERN.test(c.value)) {
+    return null;
+  } else {
+    return { alpha: { valid: false } };
+  };
+}
+
+//偶数のみ入力を許可
+function even(c: FormControl) {
+  return c.value % 2 == 0 ? null : { even: { valid: false } };
+}
+
 @Component({
   selector: 'app-hello',
   styleUrls: ['./hello.component.css'],
@@ -18,15 +33,19 @@ export class HelloComponent implements OnInit {
     this.title = 'Hello-app';
     this.message = 'FormControlを使う';
     this.myControl = new FormGroup({
-      name: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required, alpha]),
       mail: new FormControl('', [Validators.email]),
-      age: new FormControl(0, [Validators.min(1), Validators.max(150)])
+      age: new FormControl(0, [Validators.min(1), Validators.max(150), even])
     });
   }
 
   get name() { return this.myControl.get('name'); }
   get mail() { return this.myControl.get('mail'); }
   get age() { return this.myControl.get('age'); }
+
+  error(item: FornControl) {
+    return JSON.stringify(item.errors);
+  }
 
   onSubmit() {
     if (this.myControl.invalid) {
